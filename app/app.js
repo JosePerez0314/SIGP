2409f6ca01445cc35dc043344ac954bb5ffd4657
José Pérez <jotangamers@gmail.com>
2025-02-13 16:51:59 -0400

    Avanzando el header del home.html

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
+
 function mainFunction() {
 	var user = document.getElementById("username");
 	var pass = document.getElementById("password");
+	var form = document.getElementById('loginForm');
 
 	/*======Username field autofill events======*/
 	user.onfocus = function () {
@@ -26,8 +28,21 @@ function mainFunction() {
 		}
 	};
 	/*==========================================*/
+	/*====== Form validation and redirection ======*/
+	form.addEventListener("submit", function (event) {
+		event.preventDefault(); // Evita el envío automático
+
+		if (user.value.trim() === "" || user.value === "Username" || pass.value.trim() === "" || pass.value === "Password") {
+			alert("Por favor, completa todos los campos.");
+		} else {
+			window.location.href = "home.html"; // Redirige solo si los campos están completos
+		}
+	});
+	/*==========================================*/
+
 }
 
 window.onload = function () {
 	mainFunction();
 }
+
